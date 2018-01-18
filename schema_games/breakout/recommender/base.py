f4168e305a5266007ea4c492f0bdaa6c47046c7c
Luis Da Costa <dacosta.le@gmail.com>
2018-01-18 15:02:41 -0500

    Random Recommender deserves its own file

---
+++ schema_games/breakout/recommender/base.py
@@ -0,0 +1,44 @@
+from Queue import LifoQueue
+from abc import ABCMeta, abstractmethod
+
+from pattern.observer import Observable, Observer
+
+
+class Recommender(Observable, Observer): # , metaclass=ABCMeta
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        Observable.__init__(self)
+        self.recommendation = None
+        self.observations = LifoQueue()
+
+    @abstractmethod
+    def issue_recommendation(self):
+        """
+        Decide what recommendation to issue.
+        """
+        raise NotImplementedError()
+
+    def send_recommendation(self):
+        if self.recommendation is not None:
+            # print("[Recommender] Sending: %s" % (self.recommendation))
+            self.update_observers(self.recommendation)
+
+    def update(self, *args, **kwargs):
+        self.get_observation(*args)
+
+    def get_observation(self, obs):
+        """
+        Decide what to do when you get an observation.
+        The default implementation is to fill up a LIFO queue,
+        but this can (and should) be overwritten as needed
+
+        Args:
+            obs: an observation.
+
+        Returns:
+            Nothing.
+
+        """
+        # print("[Recommender] Got an observation")
+        self.observations.put(obs)
--- schema_games/breakout/recommender/recommender.py
+++ schema_games/breakout/recommender/recommender.py
@@ -1,48 +1,7 @@
-from Queue import LifoQueue
-from abc import ABCMeta, abstractmethod
-from pattern.observer import Observer, Observable
-
-class Recommender(Observable, Observer): # , metaclass=ABCMeta
-    __metaclass__ = ABCMeta
-
-    def __init__(self):
-        Observable.__init__(self)
-        self.recommendation = None
-        self.observations = LifoQueue()
-
-    @abstractmethod
-    def issue_recommendation(self):
-        """
-        Decide what recommendation to issue.
-        """
-        raise NotImplementedError()
-
-    def send_recommendation(self):
-        if self.recommendation is not None:
-            # print("[Recommender] Sending: %s" % (self.recommendation))
-            self.update_observers(self.recommendation)
-
-    def update(self, *args, **kwargs):
-        self.get_observation(*args)
-
-    def get_observation(self, obs):
-        """
-        Decide what to do when you get an observation.
-        The default implementation is to fill up a LIFO queue,
-        but this can (and should) be overwritten as needed
-
-        Args:
-            obs: an observation.
-
-        Returns:
-            Nothing.
-
-        """
-        # print("[Recommender] Got an observation")
-        self.observations.put(obs)
-
 import random
-import time
+
+from schema_games.breakout.recommender.base import Recommender
+
 
 class RandomRecommender(Recommender):
 
