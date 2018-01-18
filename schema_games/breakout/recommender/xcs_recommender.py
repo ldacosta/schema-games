c349c92293f9849a6d1091a95a962841369ca7b7
Luis Da Costa <dacosta.le@gmail.com>
2018-01-18 16:57:41 -0500

    Template 0.1 for recommender based on XCS

---
+++ schema_games/breakout/recommender/xcs_recommender.py
@@ -0,0 +1,21 @@
+import numpy as np
+from schema_games.breakout.recommender.base import Recommender
+
+
+class XCSRecommender(Recommender):
+
+    def __init__(self, env):
+        Recommender.__init__(self)
+        self.env = env
+
+    def issue_recommendation(self):
+        if not self.observations.empty():
+            latest_obs = self.observations.get()
+            assert type(latest_obs) == np.ndarray, "Current implementation only encodes 'raw' images"
+            # print("[RandomRecommender] I have %d observations; will give a recommendation and empty the observations." % (self.observations.qsize()))
+            while not self.observations.empty():
+                self.observations.get()
+            self.recommendation = self.env.action_space.sample() # TODO!!!
+            if False: # TODO, obviously
+                self.send_recommendation()
+
