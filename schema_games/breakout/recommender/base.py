afebcdd68930b9c264a4608dc0befdf126c915e5
Luis Da Costa <dacosta.le@gmail.com>
2018-01-22 12:05:22 -0500

    WIP: modifying architecture to make it work with XCS

--- schema_games/breakout/recommender/base.py
+++ schema_games/breakout/recommender/base.py
@@ -1,3 +1,4 @@
+import datetime
 from queue import LifoQueue
 from abc import ABCMeta, abstractmethod
 
@@ -11,10 +12,21 @@ class Recommender(Observable, Observer): # , metaclass=ABCMeta
         Observable.__init__(self)
         self.recommendation = None
         self.observations = LifoQueue()
+        self.rewards = LifoQueue() # each element is a tuple (timestamp (in seconds since the epoch), reward)
+
+    def timestamp(self) -> float:
+        """timestamp (in seconds since the epoch)"""
+        return datetime.datetime.now().timestamp()
+
+    def __empty_queue__(self, q):
+        with q.mutex: # make this thread safe
+            q.queue.clear()
 
     def empty_observations(self):
-        with self.observations.mutex: # make this thread safe
-            self.observations.queue.clear()
+        self.__empty_queue__(q = self.observations)
+
+    def empty_rewards(self):
+        self.__empty_queue__(q = self.rewards)
 
     @abstractmethod
     def issue_recommendation(self):
@@ -23,7 +35,9 @@ class Recommender(Observable, Observer): # , metaclass=ABCMeta
         """
         raise NotImplementedError()
 
-    def send_recommendation(self):
+    def send_recommendation(self, recommendation_to_set = None):
+        if recommendation_to_set is not None:
+            self.recommendation = recommendation_to_set
         if self.recommendation is not None:
             # print("[Recommender] Sending: %s" % (self.recommendation))
             self.update_observers(self.recommendation)
@@ -47,3 +61,18 @@ class Recommender(Observable, Observer): # , metaclass=ABCMeta
         # print("[Recommender] Got an observation")
         self.observations.put(obs)
         self.issue_recommendation()
+
+    def get_reward(self, reward):
+        """
+        Decide what to do when you get an observation.
+        The default implementation is to fill up a LIFO queue,
+        but this can (and should) be overwritten as needed
+
+        Args:
+            obs: an observation.
+
+        Returns:
+            Nothing.
+
+        """
+        self.rewards.put((self.timestamp(), reward))
--- schema_games/breakout/recommender/xcs_recommender.py
+++ schema_games/breakout/recommender/xcs_recommender.py
@@ -1,12 +1,24 @@
+import datetime
+import time
 import numpy as np
 from schema_games.breakout.recommender.base import Recommender
+from schema_games.breakout.play import Player
+from xcs.scenarios import Scenario
 
+from enum import Enum, auto
+class RunningMode(Enum):
+    TRAINING = auto() # to train the XCS system
+    PURE_RECOMMENDER = auto() # to run as a recommender, without training
 
-class XCSRecommender(Recommender):
+class XCSRecommender(Recommender, Scenario):
 
-    def __init__(self, env):
+    def __init__(self, env, mode: RunningMode):
         Recommender.__init__(self)
         self.env = env
+        if mode == RunningMode.TRAINING:
+            # start player
+            p = Player(env, recommender=self)
+            p.play_game(fps=30) # TODO: do we want to accelerate the game? Put this a number > 30
 
     def issue_recommendation(self):
         if not self.observations.empty():
@@ -18,3 +30,51 @@ class XCSRecommender(Recommender):
             if False: # TODO, obviously
                 self.send_recommendation()
 
+    # /////// Methods defined for it to be a Scenario
+    @property
+    def is_dynamic(self):
+        return True
+
+    def get_possible_actions(self):
+        return self.possible_actions
+
+    def reset(self):
+        # not sure what to do. Or even if I have to do anything. TODO?
+        logging.debug("not sure what to do. Or even if I have to do anything. TODO?")
+
+    def more(self):
+        return not self.env_done
+
+    def sense(self):
+        latest_obs = self.observations.get() # if this fails: the environment is not running or it's not updating me.
+        self.empty_observations() # I used the observations, leave space for new one.
+        return latest_obs
+
+    def execute(self, action):
+        self.empty_rewards() # old rewards do not interest me - I just want to wait for the NEW one.
+        self.send_recommendation(recommendation_to_set=action)
+        # since I know (do I?) that the simulation is running on another thread,
+        # I can block until I hear from it:
+        return self.rewards.get(block=True, timeout=1) # I lose patience - won't block forever!
+        # if the above doesn't work, you can always try:
+        # small_time_in_secs = 0.1
+        # while self.rewards.empty():
+        #     time.sleep(small_time_in_secs)
+        # return self.rewards.get(block=False) # I know the list is not empty, so I don't want to block.
+
+if __name__ == '__main__':
+    """
+    Trains an XCSRecommender.
+    """
+    from xcs import XCSAlgorithm
+    from xcs.scenarios import ScenarioObserver
+
+    breakout_env =
+    scenario = ScenarioObserver(XCSRecommender(breakout_env, mode=RunningMode.TRAINING))
+    algorithm = XCSAlgorithm()
+    # algorithm.exploration_probability = .1
+    # algorithm.discount_factor = 0
+    # algorithm.do_ga_subsumption = True
+    # algorithm.do_action_set_subsumption = True
+    model = algorithm.new_model(scenario)
+    model.run(scenario, learn=True)
